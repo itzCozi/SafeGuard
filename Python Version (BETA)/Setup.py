187e059415fc6d72bbff7906cf8d49365a9dc97e
Cooper <Cooperransom08@gmail.com>
2023-02-09 23:52:18 -0500

    added setup file

---
+++ Python Version (BETA)/Setup.py
@@ -0,0 +1,12 @@
+# How to chcek if path exists in python
+"""
+import os
+if not os.path.exists('my_folder'):
+    return False
+"""
+# how to check user name in python
+"""
+import os
+
+username = os.getlogin()
+"""
