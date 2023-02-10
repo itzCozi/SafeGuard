ad4dea3db621521e2062ceabbe8208bc389e139e
Cooper <Cooperransom08@gmail.com>
2023-02-10 18:16:35 -0500

    FIx

--- data/safeguard-files/Setup.py
+++ data/safeguard-files/Setup.py
@@ -1,15 +1,3 @@
-# How to chcek if path exists in python
-"""
-import os
-if not os.path.exists('my_folder'):
-    return False
-"""
-# how to check user name in python
-"""
-import os
-
-username = os.getlogin()
-"""
 from setuptools import setup, find_packages
 
 setup(
