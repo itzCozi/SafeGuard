fb4006e7ff6699aa05c3bdc998f3d4f32e8a52f0
Cooper <Cooperransom08@gmail.com>
2023-02-14 08:29:07 -0500

    upload new release

--- data/safeguard-files/PreChecks.py
+++ data/safeguard-files/PreChecks.py
@@ -92,7 +92,6 @@ def hashFileURL(url):
 
 
 def CUSTOMinstall(URL, Destination, NewName, FileExt=""):
-
   # Download and write to file
   file_content = requests.get(URL)
   open(Destination + '/' + NewName + FileExt, "wb").write(file_content.content)
@@ -209,5 +208,6 @@ def autoUpdate():
       f.write("SafeGuard !UP-TO-DATE! - AT: " + now)
     if debug:
       print(Fore.GREEN + "SafeGuard !UP-TO-DATE! - AT: " + now + Style.RESET_ALL)
-      clear()
+    sleep()
+    clear()
   
