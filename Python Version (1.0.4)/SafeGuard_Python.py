896497d5407966bf2a7d098d9e659afbc60185d1
Cooper <Cooperransom08@gmail.com>
2023-02-16 10:58:56 -0500

    add todo

--- Python Version (1.0.4)/SafeGuard_Python.py
+++ Python Version (1.0.4)/SafeGuard_Python.py
@@ -1,7 +1,7 @@
 # Make a program to check the known directorys for folders which usally hold viruses (Windows/Temp, roaming/Peer2Profit, All other malawarebytes detections) PYTHON PORT FROM C++
 """
 TODO: Fix all if debug: statment (Indentation Error)
-TODO: Review and refine code
+TODO: Release version 1.0.4 after code refine/reformat
 TODO: Over engineer the shit outta this
 TODO: Find a solution to the C++ problem
 """
