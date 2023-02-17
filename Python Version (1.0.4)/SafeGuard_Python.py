5d38c9da52be2006c48fe78e83be607587f63364
Cooper <Cooperransom08@gmail.com>
2023-02-16 21:53:23 -0500

    fix all todo's

--- Python Version (1.0.4)/SafeGuard_Python.py
+++ Python Version (1.0.4)/SafeGuard_Python.py
@@ -1,9 +1,7 @@
 # Make a program to check the known directorys for folders which usally hold viruses (Windows/Temp, roaming/Peer2Profit, All other malawarebytes detections) PYTHON PORT FROM C++
 """
-TODO: Test V1.0.4 on windows pc when home (with debug off)
-TODO: Release version 1.0.4 after test (2/16/23)
 TODO: Ask about C++ problem on stackoverflow (https://stackoverflow.com/a/20372880)
-TODO: Review/refine PreChecks.py and add to 1.0.4
+TODO: Fix C++ problem
 """
 
 # Imports
